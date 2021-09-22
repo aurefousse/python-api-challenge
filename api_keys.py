a222a62b518735a3899281608563b3913de52338
Aurelian Fousse <aurelianfousse@Aurelians-MBP.san.rr.com>
2021-09-21 23:17:48 -0700

    adding homework

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = 'e134e87c9a940ee934cce8cc04925a40'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = 'AIzaSyAwfYRe8c6t8Km_2ReqXHJPclH6rV3EhSg'
